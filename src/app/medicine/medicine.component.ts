b2d3be9aac19fb44a2a144d9b840983ce3213412
ikcDevelopment <estuardo.wyss@yahoo.com>
2023-03-04 16:49:41 -0600

    Changes to format forms

--- src/app/medicine/medicine.component.ts
+++ src/app/medicine/medicine.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ITreeOptions, KEYS } from '@circlon/angular-tree-component';
 
 @Component({
   selector: 'app-medicine',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./medicine.component.scss']
 })
 export class MedicineComponent implements OnInit {
+    treeTitle='Medicines';
+    subtitle='';
+    deleteBtnIsHidden: boolean = true;
+    newBtnIsHidden: boolean = true;
+    nodes = [];
+    treeIconClass: string = 'i-Library';
+    mySpace:string = '  ';
+    dataTreeOptions: ITreeOptions = {
+        actionMapping: {
+            mouse:{
+            dblClick:(_tree: any, _node: any, $event: any) =>{
+                console.log(_node);
+                console.log(_node.data.name);
+            }
+            },
+            keys:{
+            [KEYS.ENTER]: (tree: any, node: any, $event: any) => {
+                console.log(node);
+                console.log(node.data.name);
+            }
+            },
+            scrollContainer: document.documentElement
+        }
+    };
 
-  constructor() { }
+    medicineForm = new FormGroup({
+        'medicineKey': new FormControl('', Validators.required),
+        'medicineName': new FormControl('', Validators.required),
+        'medicineSupplier': new FormControl('', Validators.required),
+        'medicinePrice': new FormControl('', Validators.required)
+    });
 
-  ngOnInit(): void {
-  }
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    upLoadData(): void{}
+
+    deleteData(): void{}
+
+    resetPatient(): void{}
 
 }
